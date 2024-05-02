bdecb9aa9c139e9dba9ca6e3f7866eed542e5236
Eugene Gritz <adcgame1@gmail.com>
2024-05-02 16:44:35 +0600

    Move index.html to src/index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "vite";
+import { glob } from "glob";
+
 import vue from "@vitejs/plugin-vue";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -17,6 +19,12 @@ export default defineConfig(async () => ({
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      input: glob.sync(__dirname + "/src/**/*.html"),
     },
   },
 }));
